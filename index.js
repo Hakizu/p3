f530418e3c6584966b68d48c597f957ba37f756a
Hakizu <hakisuj@gmail.com>
2020-05-23 14:19:07 +0200

    completed 3.1 Phonebook backend step1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("test")
+const express = require('express')
+const app = express()
+
+const contacts = [
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+    },
+    { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+    },
+    { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1> Trial </h1>')
+})
+  
+app.get('/api/persons', (req, res) => {
+    res.json(contacts)
+})
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`server running on port: ${port}`)
+})

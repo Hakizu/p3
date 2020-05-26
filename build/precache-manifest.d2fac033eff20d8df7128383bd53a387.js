dce6cc35501fe997808b89913a2665922e8d50a6
Hakizu <hakisuj@gmail.com>
2020-05-26 10:57:01 +0200

    changes

---
+++ build/precache-manifest.d2fac033eff20d8df7128383bd53a387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ab7ba00da280fec1f9e0197ebd7540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9bb8310701adcccb665",
+    "url": "/static/js/2.6e3beb30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e3beb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c61549c892a7915e272",
+    "url": "/static/js/main.5a7f4cdc.chunk.js"
+  },
+  {
+    "revision": "4aaf74b7fb1ed205224c",
+    "url": "/static/js/runtime-main.48c7fc5f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('type', (req, res) => {
     return [JSON.stringify(req.body)]
@@ -91,7 +93,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const port = 3001
-app.listen(port, () => {
-    console.log(`server running on port: ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`)
 })
